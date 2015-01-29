3fa352d79d6cff1f102ef0c30fc2432fb7a4f2ad
J.D. Marble <midorikid@gmail.com>
2015-01-28 20:16:56 -0800

    Corrections for easy-197.
    * Accept numbers with dashes
    * Interpret 'X' as 10
    * Clean up unused requirements

--- src/daily/easy-197.cljs
+++ src/daily/easy-197.cljs
@@ -6,31 +6,38 @@ The challenge is to validate 10-digit ISBN numbers using the check digit.
 [Full Challenge Description](https://www.reddit.com/r/dailyprogrammer/comments/2s7ezp/20150112_challenge_197_easy_isbn_validator/)
 "
   (:require
-    [dommy.core :as dommy :refer-macros [sel1]]
-    [reagent.core :as reagent :refer [atom]]
-    [ajax.core :refer [GET POST]]
-    [clojure.string :as string]))
+    [dommy.core :refer-macros [sel1]]
+    [reagent.core :as reagent :refer [atom]]))
+
+;; # Solution
+;;
+(defn char-to-num
+  "Convert a numeral character into an integer. In this case, it also works with the Roman numeral \"X\"."
+  [chr]
+  (if (= chr \X)
+    10
+    (js/parseInt chr)))
 
 (defn validate-isbn-10
-  "# Solution
+  "The magic function here is `map-indexed`, which applies a binary function to pairs of elements and their indicies.
+  The indicies are 0 to 9, so I subtract them from 10 to get 10 to 1.
 
-  The magic function here is `map-indexed`, which applies a binary function to pairs of elements and their indicies. The
-  indicies are 0 to 9, so I subtract them from 10 to get 10 to 1.
-  "
+  The threading macro `->>` would have worked instead of `as->` if it wasn't for that pesky `mod`. I like `as->` better
+  anyway because I have a visual mark of where the value goes in the next function."
   [input]
-  (and (= 10 (count input))
-    (as-> input $                           ;; bind `$` to the return value of the last function
-          (map js/parseInt $)               ;; convert the sequence of characters into one of integers
-          (map-indexed #(* (- 10 %1) %2) $) ;; multiply each integer by its "weight"
-          (reduce + $)                      ;; sum the integers
-          (mod $ 11)
-          (= 0 $))))
+  (as-> input $                           ;; bind `$` to the return value of the last function
+        (filter #(not= \- %) $)
+        (map char-to-num $)               ;; convert the sequence of characters into one of integers
+        (map-indexed #(* (- 10 %1) %2) $) ;; multiply each integer by its "weight"
+        (reduce + $)                      ;; sum the integers
+        (mod $ 11)
+        (= 0 $)))
 
 ;; ## Result
 ;;
 ;; You can try it out with one input string at a time using the following field.
 (defn single-input []
- (let [value (atom "0345302567")]
+ (let [value (atom "0-3453-0256-7")]
    (fn []
     [:div
      [:p "Input: "
